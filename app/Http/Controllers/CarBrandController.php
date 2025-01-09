92dae6e352990f8c9975e10c8c560762b389e8ef
Lucas chen <lucas.chen@gbo-asia.com>
2025-01-09 13:50:13 +0800

    调整 CarBrandController 的 findAll 方法

--- app/Http/Controllers/CarBrandController.php
+++ app/Http/Controllers/CarBrandController.php
@@ -10,32 +10,38 @@ use Illuminate\Routing\Controller;
 
 /*
  * 汽车品牌控制器
- * */
+ **/
 class CarBrandController extends Controller
 {
     //获取汽车品牌列表
     public function findAll()
     {
-        $carBrands = CarBrands::all();
-        if (!$carBrands){
-            return response()->json([
-                'status' => 'error',
-                'message' => '汽车品牌不存在'
-            ],500);
-        }
-        return response()->json($carBrands,200);
+        $carBrands = CarBrands::select([
+            "brand_id as id",
+            "brand_name as label",
+            "pinyin"
+        ])->get();
+
+        // if (!$carBrands) {
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => '汽车品牌不存在'
+        //     ], 500);
+        // }
+
+        return response()->json($carBrands, 200);
     }
 
     //获取汽车品牌旗下的系列
     public function findSeries($brand_id)
     {
         $carBrands = CarBrandSeries::where('brand_id', $brand_id)->get();
-        if ($carBrands->isEmpty()){
+        if ($carBrands->isEmpty()) {
             return response()->json([
                 'status' => 'error',
                 'message' => '汽车品牌不存在'
-            ],500);
+            ], 500);
         }
-        return response()->json($carBrands,200);
+        return response()->json($carBrands, 200);
     }
 }
